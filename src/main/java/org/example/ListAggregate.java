2aa6e5bd98aeb0832dc4be6e51ee53c5342f4375
Marko Topolnik <marko@hazelcast.com>
2020-07-20 15:55:46 +0200

    Improved source performance

--- src/main/java/org/example/ListAggregate.java
+++ src/main/java/org/example/ListAggregate.java
@@ -1,49 +1,23 @@
 package org.example;
 
-import com.hazelcast.jet.Jet;
-import com.hazelcast.jet.JetInstance;
-import com.hazelcast.jet.Job;
-import com.hazelcast.jet.pipeline.BatchSource;
-import com.hazelcast.jet.pipeline.Pipeline;
 import com.hazelcast.jet.pipeline.Sinks;
-import com.hazelcast.jet.pipeline.SourceBuilder;
-
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.LongStream;
 
 import static com.hazelcast.jet.aggregate.AggregateOperations.toList;
+import static org.example.LongSource.longStage;
+import static org.example.Main.runJetBenchmark;
 
 public class ListAggregate {
 
-    private static final long RANGE = 100_000_000;
     private static final long NUM_KEYS = 50_000_000;
+    private static final long RANGE = 100_000_000;
 
     public static void main(String[] args) {
-        JetInstance jet = Jet.bootstrappedInstance();
-        Pipeline p = Pipeline.create();
-
-        p.readFrom(longSource(RANGE))
-         .groupingKey(n -> n % NUM_KEYS)
-         .aggregate(toList())
-         .filter(x -> (x.getKey() % NUM_KEYS) == 1_000_000)
-         .writeTo(Sinks.logger());
-
-        Job job = jet.newJob(p);
-        job.join();
-    }
-
-    private static BatchSource<Long> longSource(Long end) {
-        return SourceBuilder
-                .batch("longs", c -> LongStream.range(0, end).boxed().iterator())
-                .<Long>fillBufferFn((longs, buf) -> {
-                    for (int i = 0; i < 128 && longs.hasNext(); i++) {
-                        longs.next();
-                        buf.add(ThreadLocalRandom.current().nextLong(end));
-                    }
-                    if (!longs.hasNext()) {
-                        buf.close();
-                    }
-                })
-                .build();
+        runJetBenchmark(p -> {
+            longStage(p, "input", NUM_KEYS, RANGE)
+                    .groupingKey(n -> n)
+                    .aggregate(toList())
+                    .filter(x -> (x.getKey() % NUM_KEYS) == 1_000_000)
+                    .writeTo(Sinks.logger());
+        });
     }
 }
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.pipeline.Pipeline;
+
+import java.util.function.Consumer;
+
+class Main {
+    static void runJetBenchmark(Consumer<? super Pipeline> buildPipelineFn) {
+        Pipeline p = Pipeline.create();
+        buildPipelineFn.accept(p);
+        JetInstance jet = Jet.bootstrappedInstance();
+        try {
+            jet.newJob(p).join();
+        } finally{
+            Jet.shutdownAll();
+        }
+    }
+
+}
